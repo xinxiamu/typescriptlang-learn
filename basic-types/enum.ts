cfa609ac67476fa5c44ab1524e34174e753929a5
xinxiamu <932852117@qq.com>
2019-07-06 00:30:23 +0800

    学习

--- basic-types/enum.ts
+++ basic-types/enum.ts
@@ -3,7 +3,7 @@
 // enum Color {Red = 1, Green, Blue} //手动设置编号
 enum Color {Red = 1, Green = 2, Blue = 4}  //全部手动设置编号
 
-let c: Color = Color.Green;
+let c: Color = Color.Red;
 let c1: string = Color[4];
 
 function showEnum() {
