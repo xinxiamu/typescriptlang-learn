ca956292b57aabc20b34cbf62d00a5616e4ddd75
xinxiamu <932852117@qq.com>
2019-11-06 22:34:41 +0800

    学习

--- generics/generics-start.ts
+++ generics/generics-start.ts
@@ -30,3 +30,55 @@ let output = identity("myString");  // type of output will be 'string'
 function testGenerics1() {
     alert(">>>>>>>>>" + output);
 }
+
+//=====泛型类型
+//泛型函数的类型与非泛型函数的类型没什么不同，只是有一个类型参数在最前面，像函数声明一样：
+function identity1<T>(arg: T): T {
+    return arg;
+}
+
+let myIdentity: <T>(arg: T) => T = identity1;
+//我们也可以使用不同的泛型参数名，只要在数量上和使用方式上能对应上就可以。
+// let myIdentity: <U>(arg: U) => U = identity;
+//我们还可以使用带有调用签名的对象字面量来定义泛型函数：
+// let myIdentity: {<T>(arg: T): T} = identity;
+
+function testGeneratics2() {
+    alert(">>>>" + myIdentity(123));
+    alert(">>>>" + identity1("abc"));
+}
+
+//======泛型接口
+interface GenericIdentityFn<T> {
+    (arg: T): T;
+}
+
+function identity3<T>(arg: T): T {
+    return arg;
+}
+
+// let myIdentity3: GenericIdentityFn<number> = identity3;
+let myIdentity3: GenericIdentityFn<number> = identity3;
+
+function testGenerics3() {
+    alert(">>>>>>>>>" + myIdentity3(123));
+}
+
+//==========泛型类
+class GenericNumber<T> {
+    zeroValue: T;
+    add: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function(x, y) { return x + y; };
+
+let stringNumeric = new GenericNumber<string>();
+stringNumeric.zeroValue = "aa";
+stringNumeric.add = function(x, y) { return x + y; };
+
+function testGenerics4() {
+    alert(myGenericNumber.add(2,3))
+    alert(stringNumeric.add(stringNumeric.zeroValue, "test"));
+}
