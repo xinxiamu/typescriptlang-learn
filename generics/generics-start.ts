080af34c57ab946adb319218f7ee153b61a1aa1e
xinxiamu <932852117@qq.com>
2019-11-08 00:23:59 +0800

    学习

--- generics/generics-start.ts
+++ generics/generics-start.ts
@@ -65,6 +65,7 @@ function testGenerics3() {
 }
 
 //==========泛型类
+//泛型类指的是实例部分的类型，所以类的静态属性不能使用这个泛型类型。
 class GenericNumber<T> {
     zeroValue: T;
     add: (x: T, y: T) => T;
@@ -82,3 +83,68 @@ function testGenerics4() {
     alert(myGenericNumber.add(2,3))
     alert(stringNumeric.add(stringNumeric.zeroValue, "test"));
 }
+
+//=============泛型约束
+// function loggingIdentity<T>(arg: T): T {
+//     console.log(arg.length);  // Error: T doesn't have .length
+//     return arg;
+// }
+//上面函数泛型没有约束，并不是每个T都有.length属性。
+//下面添加上泛型约束，约束该泛型是某一类泛型。
+interface Lengthwise {
+    length: number;
+}
+//参数是类型Lengthwise
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length);  // Now we know it has a .length property, so no more error
+    return arg;
+}
+
+// loggingIdentity(3);  // Error, number doesn't have a .length property
+// loggingIdentity({length: 10, value: 3});
+
+
+//=======在泛型约束中使用类型参数
+//你可以声明一个类型参数，且它被另一个类型参数所约束。 比如，现在我们想要用属性名从对象里获取这个属性。 并且我们想要确保这个属性存在于对象 obj上，因此我们需要在这两个类型之间使用约束。
+// function getProperty(obj: T, key: K) {
+//     return obj[key];
+// }
+//
+// let x = { a: 1, b: 2, c: 3, d: 4 };
+//
+// getProperty(x, "a"); // okay
+// getProperty(x, "m"); // error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd'.
+
+//==========在泛型里使用类类型
+//在TypeScript使用泛型创建工厂函数时，需要引用构造函数的类类型。比如，
+function create<T>(c: {new(): T; }): T {
+    return new c();
+}
+
+//一个更高级的例子，使用原型属性推断并约束构造函数与类实例的关系。
+class BeeKeeper { //养蜂人
+    hasMask: boolean;
+}
+
+class ZooKeeper { //动物园管理员
+    nametag: string;
+}
+
+class Animal { //动物
+    numLegs: number;
+}
+
+class Bee extends Animal { //蜜蜂
+    keeper: BeeKeeper;
+}
+
+class Lion extends Animal { //狮子
+    keeper: ZooKeeper;
+}
+
+function createInstance<A extends Animal>(c: new () => A): A {
+    return new c();
+}
+
+createInstance(Lion).keeper.nametag;  // typechecks!
+createInstance(Bee).keeper.hasMask;   // typechecks!
